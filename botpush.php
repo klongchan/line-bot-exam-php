6999cd6a6f22eb68b9198c42e0457b9f794c9f77
klongchan <40650904+klongchan@users.noreply.github.com>
2018-06-28 11:18:54 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -6,7 +6,7 @@ require "vendor/autoload.php";
 
 $access_token = 'OEEClWngIWeKNxZCyuAMUZrGr1nwSAAPw2YYP0sIT0kLByOxvjHv1K4vZ5fb5HiJWV3XiPzyvM2pvkk/VjKzsdMyns6/RwYuNX8lh1IcEbC5igL8yjbqlqtETUmFC+kxCpZ3ZoAoWmm9M/I9cMRRAQdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '6f82f5b967bf535adb280873ccdc3fbb';
 
 $pushID = 'U7b1ec2efb3d533ffd37440a0526b900c';
 
